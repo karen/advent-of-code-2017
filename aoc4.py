61e92185018cfc213d4550c72fc6b369e3c6e02a
Karen Ang <thekarenang@gmail.com>
2017-12-04 13:16:47 +0800

    Day 4

---
+++ aoc4.py
@@ -0,0 +1,17 @@
+def solve():
+    with open('aoc4.txt', 'r') as f:
+        return len(list(filter(lambda line: len(line.split()) == len(set(line.split())), f)))
+
+def solve2():
+    res = 0
+    with open('aoc4.txt', 'r') as f:
+        for line in f:
+            sorted_data = list(map(lambda x: ''.join(sorted(x)), line.split()))
+            if len(line.split()) == len(set(sorted_data)):
+                res += 1
+    return res
+
+if __name__ == '__main__':
+    res = 0
+    print(solve())
+    print(solve2())
